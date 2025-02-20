02861abec66c83b89f69ab7a3c106a871ffcb3b9
Arafat-Forhaz <arafat69forhaz@gmail.com>
2025-02-20 12:00:56 +0600

    commit

---
+++ index.js
@@ -0,0 +1,139 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json()) 
+
+// Morgan Middleware configuration
+const postexception = (mrgn) => {
+    return (req, res, next) => {
+        if (req.method === 'POST') {
+            next()
+        } else {
+            mrgn(req, res, next)
+        }
+    }
+}
+
+app.use(postexception(morgan('tiny')))
+
+morgan.token("body" , req => JSON.stringify(req.body))
+
+
+
+let persons = 
+[
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+
+
+
+app.get('/', (request, response) => {
+    response.send("Hello")
+})
+
+app.get('/info', (request, response) => {
+    const date = new Date()
+
+    response.send(`
+        <p>Phonebook has info for ${persons.length} persons</p>
+        <p>${date}</p>
+    `)
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons .find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+    
+    response.status(204).end()
+})  
+
+const postMorgan = morgan(':method :url :status :res[content-length] - :response-time ms :body')
+
+const generateId = () => {
+    const randNum = persons.length > 0 ?
+        Math.floor(Math.random() * 10000) :
+        1
+    
+    id = randNum + persons.length
+    return id
+}
+
+app.post('/api/persons', postMorgan, (request, response) => {
+
+    const body = request.body
+
+    if (!body.name && !body.number) {
+        return response.status(400).json({
+            error : "name and number missing"
+        })
+    } else if (!body.name) {
+        return response.status(400).json({
+            error : "name missing"
+        })
+    } else if (!body.number) {
+        return response.status(400).json({
+            error : "number missing"
+        })
+    } else if (persons.some(p => p.name === body.name)) {
+        return response.status(403).json({
+            error : "name must be unique"
+        })
+    }
+
+    const person = {
+        name : body.name ,
+        number : body.number ,
+        id : randNum
+    }
+    
+    persons.concat(person)
+
+    console.log(person)
+    response.json(person)
+})  
+
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
