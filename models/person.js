406623846525555d04d1490ec27897e2ff0e1be0
Arafat-Forhaz <arafat69forhaz@gmail.com>
2025-03-08 11:54:57 +0600

    finally

--- models/person.js
+++ models/person.js
@@ -4,8 +4,6 @@ const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery',false)
 
-console.log('connecting to', url)
-
 mongoose.connect(url)
   .then(() => {console.log('connected to MongoDB')})
   .catch((error) => {console.log('error connecting to MongoDB:', error.message)})
